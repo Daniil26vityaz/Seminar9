bbcfa5ea238c5c745fb1781f047139b78c7f9d1b
Daniil <mr.varfolomeev@bk.ru>
2023-08-14 19:33:39 +0300

    finished with  the homework for Seminar9

--- tasks/Program.cs
+++ tasks/Program.cs
@@ -18,7 +18,7 @@ class Program
                 {
                     return result;
                 }
-                
+
                 if (M % 3 == 0)
                 {
                     result = M;
@@ -32,7 +32,7 @@ class Program
             int number1 = MyLibClass.Input("Введите число M: ");
             int number2 = MyLibClass.Input("Введите число N: ");
             Console.Write($"числа в промежутке от {number1} до {number2} кратные 3-ем: ");
-            Recursion(number1,number2);
+            Recursion(number1, number2);
             Console.Write("\b\b  ");
 
             // int result = 0;
@@ -49,7 +49,8 @@ class Program
 
         }
 
-        Task64();
+        // Task64();
+        Console.Clear();
 
         // Задача 66: Задайте значения M и N. Напишите рекурсивный метод, 
         // который найдёт сумму натуральных элементов в промежутке от M до N.
@@ -59,10 +60,35 @@ class Program
 
         void Task66()
         {
+            int Recursion(int M, int N, int result = 0)
+            {
+                if (M > N)
+                {
+                    Console.Write($"{result}, ");
+                    Console.Write("\b\b  ");
+                    return result;
+                }
+                result += M;
 
+                M++;
+                return Recursion(M, N, result);
+            }
+            int number1 = MyLibClass.Input("Введите число M: ");
+            int number2 = MyLibClass.Input("Введите число N: ");
+            Console.Write($"сумма натуральных элементов в промежутке от {number1} до {number2} равна = ");
+            Recursion(number1, number2);
+            // int result = 0;
+            // Console.Write($"сумма натуральных элементов в промежутке от {number1} до {number2} равна = ");
+            // for (int i = number1; i <= number2; i++)
+            // {                
+            //         result += i;            
+            // }
+            // Console.Write($"{result}, ");
+            // Console.Write("\b\b  ");
         }
 
-        Task66();
+        // Task66();
+        Console.Clear();
 
         // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
         // Даны два неотрицательных числа m и n.
@@ -72,9 +98,44 @@ class Program
         void Task68()
         {
 
-        }
+            int Recursion(int m, int n)
+            {
+                if (m == 0)
+                {
+                    return n + 1;
+                }
+                if (m != 0 && n == 0)
+                {
+                    return Recursion(m - 1, n = 1);
+                }
+                if (m > 0 || n > 0)
+                {
+                    return Recursion(m - 1, Recursion(m, n - 1));
+                }
+                return Recursion(m, n);
+            }
 
+
+            int number1 = MyLibClass.Input("Введите координату m: ");
+            int number2 = MyLibClass.Input("Введите координату n: ");
+
+            if (number1 >= 0 && number2 >= 0)
+            {
+                Console.Write($"A ({number1}, {number2}) равна = ");
+                Console.Write(Recursion(number1, number2));
+            }
+            else
+            {
+                Console.Write($"m и n должны быть неторицательными!!!");
+            }
+
+        }
         Task68();
 
+
     }
+
+
+
 }
+
